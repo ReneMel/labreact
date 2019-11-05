6652967127f74e6827cd60295a7b2379a30f6b31
renemel <rene.melendeza@gmail.com>
2019-11-04 20:45:21 -0600

    Updates

---
+++ precache-manifest.b72471bb78b7c8423f37874d7eeccc72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa842af91100e3ef8b896e33b2cf2f84",
+    "url": "/labreact/index.html"
+  },
+  {
+    "revision": "50d0ef243e235aabd35e",
+    "url": "/labreact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "be9524226ddeefbb9d29",
+    "url": "/labreact/static/js/2.7044d5e7.chunk.js"
+  },
+  {
+    "revision": "50d0ef243e235aabd35e",
+    "url": "/labreact/static/js/main.b03cce27.chunk.js"
+  },
+  {
+    "revision": "0110d2237097850f7c35",
+    "url": "/labreact/static/js/runtime-main.edb29def.js"
+  }
+]);
